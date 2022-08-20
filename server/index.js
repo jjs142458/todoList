d6a7bd68a78b9b94b1717368a18d32a540c2b13f
jaesung <jjs142458@naver.com>
2022-08-21 03:15:53 +0900

    jwt

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require("path");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-const app = express();
-const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("씨발 기본 설정 ㅈ같네 git 연결 왜 안됌?");
-});
+const app = express();
+
+const { auth } = require("./authMiddleware");
+const { json } = require("express");
+
+const port = 8080;
+const SECRET_KEY = "MY-SECRET-KEY";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   mongoose
@@ -17,4 +24,41 @@ app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     })
     .catch((err) => console.log(err));
+});
+
+// POST /login 요청 body에 id와 password를 함께 실어서 요청으로 가정 (사실 id와 password는 암호화 되어있음)
+app.post("/login", (req, res, next) => {
+  const nickName = "jeasung";
+  const profile = "imageURL";
+
+  //jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  token = jwt.sign(
+    {
+      type: "JWT",
+      nickname: nickName,
+      profile: profile,
+    },
+    SECRET_KEY,
+    {
+      expiresIn: "10m",
+      issuer: "토큰 발급자",
+    }
+  );
+
+  return res
+    .status(200)
+    .json({ code: 200, message: "토큰이 발급되었습니다", token: token });
+});
+
+app.get("/payload", auth, (req, res) => {
+  const nickname = req.decoded.nickname;
+  const profile = req.decoded.profile;
+  return res.status(200).json({
+    code: 200,
+    message: "토큰이 정상입니다.",
+    data: {
+      nickname: nickname,
+      profile: profile,
+    },
+  });
 });
