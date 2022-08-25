0c5ebb45be78006a03c27974d9aae4cee94c85b2
정재성 <jjs142458@naver.com>
2022-08-26 01:30:27 +0900

    update delete

--- server/index.js
+++ server/index.js
@@ -3,24 +3,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const app = express();
 const port = 8080;
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "imsosorry",
-  database: "todo",
-});
-db.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", require("./router/user.js"));
-
-let todoList = {
-  content: "mysql learning",
-  isdone: true,
-};
+app.use("/api/post", require("./router/post.js"));
 
 app.listen(port, () => {
   mongoose
@@ -32,28 +20,4 @@ app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     })
     .catch((err) => console.log(err));
-});
-
-app.post("/list", (req, res) => {
-  db.query(`SELECT * FROM todo`, (err, todoList) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(todoList);
-    db.end();
-  });
-});
-
-app.post("/submit", (req, res) => {
-  db.query(
-    `INSERT INTO todo (content, createAt, isdone) VALUES (?, NOW(), ?)`,
-    [todoList.content, todoList.isdone],
-    (err, todoList) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(todoList);
-      db.end();
-    }
-  );
 });
---
+++ server/router/post.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mjchoi12##",
+  database: "todo",
+});
+db.connect();
+
+let todoList = {
+  content: "mysql learning",
+  isdone: true,
+};
+
+router.post("/list", (req, res) => {
+  db.query(`SELECT * FROM todo;`, (err, todoList) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(todoList);
+    db.end();
+  });
+});
+
+router.post("/submit", (req, res) => {
+  db.query(
+    `INSERT INTO todo (content, createAt, isdone) VALUES (?, NOW(), ?)`,
+    [todoList.content, todoList.isdone],
+    (err, todoList) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(todoList);
+      db.end();
+    }
+  );
+});
+
+router.put("/update", (req, res) => {
+  let data = {
+    id: 3,
+    content: "씨발  ",
+    isdone: 1,
+  };
+
+  db.query(
+    `UPDATE todo SET content = ?, isdone = ? WHERE id = ?;`,
+    [data.content, data.isdone, data.id],
+    (err, todoList) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(todoList);
+      db.end();
+    }
+  );
+});
+
+router.delete("/delete", (req, res) => {
+  let data = {
+    id: 3,
+    content: "씨발  ",
+    isdone: 1,
+  };
+  db.query(`DELETE FROM todo WHERE id = ?`, [data.id], (err, todoList) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(todoList);
+  });
+});
+
+module.exports = router;
